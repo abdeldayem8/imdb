816889a2a86c0fe01f4028d1abf0f64018fdb40c
abdeldayem8 <mhmdabdeldayem40@gmail.com>
2024-03-04 19:09:50 +0200

    add header section

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Menuitem from '../Menuitem/menuitem'
+import { MdHome } from "react-icons/md";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <div className='flex justify-between itmes-center p-3 max-w-6xl mx-auto'>
+     <div className='flex gap-4'>
+        <Menuitem  title='home' address='/' Icon={MdHome} />
+        <Menuitem  title='about' address='/about' Icon={BsFillInfoCircleFill} />
+     </div>
+     <Link href={'/'} className='flex gap-1 items-center'>
+      <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IMDB</span>
+      <span className='text-xl hidden sm:inline'>Clone</span>
+     </Link> 
+    </div>
+  )
+}
