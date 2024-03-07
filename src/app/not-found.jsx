eeef2f9eb29991b94194d2840691f7204051fb45
abdeldayem8 <mhmdabdeldayem40@gmail.com>
2024-03-07 23:36:11 +0200

    del

--- src/app/not-found.jsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function Notfound() {
-  return <>
-    <div className="p-3 max-w-6xl mx-auto flex justify-center items-center min-h-screen">
-        404 | Page Not Found
-        </div>
-  </>
-}
