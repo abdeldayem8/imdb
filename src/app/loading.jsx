35d5dc442bebd92ffa0dc3b703d558a5e83bc500
abdeldayem8 <mhmdabdeldayem40@gmail.com>
2024-03-07 17:59:37 +0200

    add small changes

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,19 @@
+'use client'
 import React from 'react'
+import { RotatingTriangles } from 'react-loader-spinner'
 
 export default function Loading() {
   return <>
   <div className='flex justify-center mt-10'>
-   <img  src='spinner.svg' alt='loading...' />
+  <RotatingTriangles
+  visible={true}
+  height="80"
+  width="80"
+  color="#4fa94d"
+  ariaLabel="rotating-triangles-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  />
    </div>
   </>
 }
---
+++ src/app/not-found.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Notfound() {
+  return <>
+    <div className="p-3 max-w-6xl mx-auto flex justify-center items-center min-h-screen">
+        404 | Page Not Found
+        </div>
+  </>
+}
