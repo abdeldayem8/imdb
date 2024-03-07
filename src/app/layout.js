c03df754cc783a35892427a9ab0f452bdc54d5cd
abdeldayem8 <mhmdabdeldayem40@gmail.com>
2024-03-07 22:19:40 +0200

    add search

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header/header";
 import Providers from "./Providers";
 import Navbar from "@/components/Navbar/navbar";
+import Searchbox from "@/components/Search/searchbox";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         <Providers>
         <Header/>
         <Navbar/>
+        <Searchbox/>
         {children}
         </Providers>
         </body>
--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -8,7 +8,7 @@ export default async function Moviepage({params}) {
   return <>
    <div className="w-full">
     <div className="flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6 md:pt-8 p-3">
-    <Image src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} width={500} height={300} className="rounded-lg" />
+    <Image src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path || movie.poster_path}`} alt="image" width={500} height={300} className="rounded-lg" />
     <div  className="p-2">
     <h2 className='text-lg font-bold truncate'>{movie.title || movie.name }</h2>
     <p className="line-clamp-3 text-md my-3"><strong>Overview:</strong>{movie.overview}</p>
